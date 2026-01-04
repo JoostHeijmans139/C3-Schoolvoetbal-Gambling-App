54d2aea844dbd1af0ccc1f7f82cf4fd25f355486
Nigel Koremans <nigel.koremans@gmail.com>
2026-01-04 15:48:10 +0100

    Create PointsViewModel.cs

---
+++ GamblingApp/GamblingApp/PointsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingApp
+{
+    internal class PointsViewModel : INotifyPropertyChanged
+    {
+        private int _points = 10;
+        public int Points
+        {
+            get => _points;
+            set
+            {
+                _points = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
