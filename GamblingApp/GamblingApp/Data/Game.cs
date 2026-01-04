160b573ab4e2b01f8cefd18833482152ee85af92
Nigel Koremans <nigel.koremans@gmail.com>
2026-01-04 15:47:24 +0100

    Add data models for Game, Team, and Tournament

---
+++ GamblingApp/GamblingApp/Data/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GamblingApp.Data
+{
+    internal class Game
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("start")]
+        public DateTime Start { get; set; }
+
+        [JsonPropertyName("duration_in_minutes")]
+        public int DurationInMinutes { get; set; }
+
+        [JsonPropertyName("team_1")]
+        public Team Team1 { get; set; }
+
+        [JsonPropertyName("team_2")]
+        public Team Team2 { get; set; }
+
+        [JsonPropertyName("tournament")]
+        public Tournament Tournament { get; set; }
+
+        [JsonPropertyName("score_team_1")]
+        public int? ScoreTeam1 { get; set; }
+
+        [JsonPropertyName("score_team_2")]
+        public int? ScoreTeam2 { get; set; }
+        public string FormattedStart => Start.ToString("HH:mm ddd d MMM");
+    }
+}
---
+++ GamblingApp/GamblingApp/Data/Tournament.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GamblingApp.Data
+{
+    internal class Tournament
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("capacity")]
+        public int Capacity { get; set; }
+
+        [JsonPropertyName("start_date")]
+        public DateTime StartDate { get; set; }
+    }
+}
